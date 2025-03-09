f42c7d1e18b95d467023b0eed44e5926555bc63e
Sky282128 <hsualdrin28@gmail.com>
2025-03-09 20:35:38 +0800

    made some edits

--- controller/cr_search_username.js
+++ controller/cr_search_username.js
@@ -1,15 +1,8 @@
 const { customers } = require("../model/customer");
 
-exports.getSearchUsernamePage = (req, res) => {
-    res.render("search_username");
-};
-
 exports.getSearchUsernamePage = async (req, res) => {
     try {
-        // Fetch all users from the database
-        const users = await customers.find({}, "username pfp"); // Use "pfp" instead of "profile_pic"
-
-        // Render the EJS page with users data
+        const users = await customers.find({}, "username email pfp");
         res.render("search_username", { users });
     } catch (error) {
         console.error("Error fetching users:", error);
